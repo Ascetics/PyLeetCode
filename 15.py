8c535092776e19cad548870d1c7970cc46c8dc7f
lingmk <mingkun.ling@qq.com>
2020-12-20 10:59:31 +0800

    执行结果：通过 执行用时：588 ms, 在所有 Python3 提交中击败了95.06% 的用户 内存消耗：17.4 MB, 在所有 Python3 提交中击败了5.01% 的用户

--- 15.py
+++ 15.py
@@ -1,40 +1,63 @@
-from functools import reduce
+"""
+给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，
+使得 a + b + c = 0 ？
+请你找出所有满足条件且不重复的三元组。
 
-nums = [-1, 0, 1, 2, -1, -4]
+注意：答案中不可以包含重复的三元组。
+示例：
 
+给定数组 nums = [-1, 0, 1, 2, -1, -4]，
 
-# nums = [0, 0, 0, 0]
+满足要求的三元组集合为：
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/3sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+nums1 = [-1, 0, 1, 2, -1, -4]
+nums2 = [0, 0, 0]
 
 
 class Solution:
     def threeSum(self, nums):
+        """
+        0、nums至少有3个元素
+        1、排序
+        2、a<b<c a必须<=0，如果a>0那就结束
+        3、避免重复，a,b,c都要避免重复
+        :param nums:
+        :return:
+        """
+        if len(nums) < 3:
+            return []
         nums = sorted(nums)
-        dic = {}
         res = []
-        for n in nums:
-            v = dic.get(n)
-            if v:
-                dic[n] = v + 1
+        for i, a in enumerate(nums):
+            if a > 0:
+                return res
             else:
-                dic[n] = 1
-
-        print(nums)
-        for i, x in enumerate(nums):
-            dic[x] = dic[x] - 1
-            for j, y in enumerate(nums[i + 1:]):
-                dic[y] = dic[y] - 1
-                z = 0 - x - y
-                if z >= y:
-                    remain = dic.get(z)
-                    if remain and remain > 0:
-                        r = [x, y, z]
-                        if r not in res:
-                            res.append(r)
-                dic[y] = dic[y] + 1
-            dic[x] = dic[x] + 1
-
+                if i > 0 and a == nums[i - 1]:
+                    continue  # 避免a重复
+                left, right = i + 1, len(nums) - 1
+                while left < right:
+                    if a + nums[left] + nums[right] < 0:
+                        left += 1
+                    elif a + nums[left] + nums[right] > 0:
+                        right -= 1
+                    else:
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1  # 避免b重复
+                        while left < right and nums[right - 1] == nums[right]:
+                            right -= 1  # 避免c重复
+                        res.append([a, nums[left], nums[right]])
+                        left, right = left + 1, right - 1
         return res
 
 
 s = Solution()
-print(s.threeSum(nums))
+print(s.threeSum(nums1))
+print(s.threeSum(nums2))
