c9bc171f0a71239cc227c067a74d9c65d7dc613c
lingmk <mingkun.ling@qq.com>
2020-12-22 00:51:56 +0800

    执行结果：通过 执行用时：84 ms, 在所有 Python3 提交中击败了19.78% 的用户 内存消耗：15.1 MB, 在所有 Python3 提交中击败了5.15% 的用户

--- src/746.py
+++ src/746.py
@@ -9,7 +9,7 @@
 # 输出: 15
 # 解释: 最低花费是从cost[1]开始，然后走两步即可到阶梯顶，一共花费15。
 # 
-#  示例 2:
+# 示例 2:
 # 输入: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
 # 输出: 6
 # 解释: 最低花费方式是从cost[0]开始，逐个经过那些1，跳过cost[3]，一共花费6。
@@ -22,24 +22,44 @@
 # 链接：https://leetcode-cn.com/problems/min-cost-climbing-stairs
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
+
 class Solution:
 
     def minCostClimbingStairs(self, cost) -> int:
         """
         cost[i]连接cost[i+1]和cost[i+2]，边的长度为cost[i]
         这样连起来的图，选择最短路径
+        从cost[0]出发走一次，从cost[1]出发走一次，取两者较小的
         """
-        pass
+        # 从cost[0]出发走一次
+        cost0 = cost
+        acc0 = [0] * (1 + len(cost0))
+        init0 = [True] * (1 + len(cost0))
+        for i, _ in enumerate(cost0):
+            if i + 1 <= len(cost0) and (init0[i + 1] or acc0[i] + cost0[i] < acc0[i + 1]):
+                acc0[i + 1], init0[i + 1] = acc0[i] + cost0[i], False                 
+            if i + 2 <= len(cost0) and (init0[i + 2] or acc0[i] + cost0[i] < acc0[i + 2]):
+                acc0[i + 2], init0[i + 2] = acc0[i] + cost0[i], False
+           
+        # 从cost[1]出发走一次
+        cost1 = cost[1:]
+        acc1 = [0] * (1 + len(cost1))
+        init1 = [True] * (1 + len(cost1))
+        for i, _ in enumerate(cost1):
+            if i + 1 <= len(cost1) and (init1[i + 1] or acc1[i] + cost1[i] < acc1[i + 1]):
+                acc1[i + 1], init1[i + 1] = acc1[i] + cost1[i], False
+            if i + 2 <= len(cost1) and (init1[i + 2] or acc1[i] + cost1[i] < acc1[i + 2]):
+                acc1[i + 2], init1[i + 2] = acc1[i] + cost1[i], False
+        
+        return acc0[-1] if acc0[-1] < acc1[-1] else acc1[-1]
 
     
 if __name__ == '__main__':
-    cost0 = []
     cost1 = [10, 15, 20]
     cost2 = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
     cost3 = [0, 0, 0, 0]
     
     sol = Solution()
-    print(cost0, sol.minCostClimbingStairs(cost0), sep='\n')
     print(cost1, sol.minCostClimbingStairs(cost1), sep='\n')
     print(cost2, sol.minCostClimbingStairs(cost2), sep='\n')
     print(cost3, sol.minCostClimbingStairs(cost3), sep='\n')
