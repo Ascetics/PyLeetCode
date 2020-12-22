b17e0f5645e44a7e876243c7d301c6598b653dea
lingmk <mingkun.ling@qq.com>
2020-12-22 21:18:44 +0800

    执行结果：通过
    执行用时：28 ms, 在所有 Python3 提交中击败了99.75% 的用户
    内存消耗：15.2 MB, 在所有 Python3 提交中击败了5.03% 的用户

--- src/103.py
+++ src/103.py
@@ -1,33 +1,31 @@
-'''
-103. 二叉树的锯齿形层序遍历
-
-给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
-
-例如：
-给定二叉树 [3,9,20,null,null,15,7],
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-
-    3
-   / \
-  9  20
-    /  \
-   15   7
-
-返回锯齿形层序遍历如下：
-
-[
-  [3],
-  [20,9],
-  [15,7]
-]
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-'''
+# 103. 二叉树的锯齿形层序遍历
+# 
+# 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+# 
+# 例如：
+# 给定二叉树 [3,9,20,null,null,15,7],
+# 
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+# 
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+# 
+# 返回锯齿形层序遍历如下：
+# 
+# [
+#   [3],
+#   [20,9],
+#   [15,7]
+# ]
+# 
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 
 # Definition for a binary tree node.
@@ -44,40 +42,45 @@ class Solution:
     def zigzagLevelOrder(self, root: TreeNode):
         """
         特判，如果root为空，那么返回[]
+        
         root进入队列q，下一层队列q_sub和结果r都为[]，方向最初是向右
+        
         反向遍历q,相当于队列出队，进入r。
         判断方向向右，先加左再加右。得到本层结果r和下一层队列的反向q_sub
         更新队列q和方向进入下一层。
+        
         上一层得到的是反向的队列，反向遍历顺序还是对的。（第一层因为只有1个node，所以正反一样）
         判断方向向左，先加右再加左。
+        
         周而复始，直到队列q为空。        
         """
-        if not root: return []
-        q, right, res = [root], True, []
+        if not root: 
+            return []
+        q, left_to_right, res = [root], True, []
         while len(q) > 0:
             q_sub, r = [], []
             for n in reversed(q):
                 r.append(n.val)
-                if right:
+                if left_to_right:
                     if n.left: q_sub.append(n.left)
                     if n.right: q_sub.append(n.right)
                 else:
                     if n.right: q_sub.append(n.right)
                     if n.left: q_sub.append(n.left)
             if len(r) > 0: res.append(r)
-            q, right = q_sub, not right
+            q, left_to_right = q_sub, not left_to_right
         return res
         
         
 if __name__ == '__main__':
     root0 = None
     
-    left, right = TreeNode(15), TreeNode(7)
+    left, left_to_right = TreeNode(15), TreeNode(7)
     root = TreeNode(20)
-    root.left, root.right = left, right
-    left, right = TreeNode(9), root
+    root.left, root.right = left, left_to_right
+    left, left_to_right = TreeNode(9), root
     root = TreeNode(3)
-    root.left, root.right = left, right
+    root.left, root.right = left, left_to_right
     root1 = root
     
     root2 = TreeNode(17)
